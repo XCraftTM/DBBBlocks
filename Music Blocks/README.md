d83e2f2e2c87ca09c5b76908a18b6c104da07e06
XCraftTM <erikselesnev31@gmail.com>
2023-03-12 15:19:08 +0100

    Added new Blocks

---
+++ Music Blocks/README.md
@@ -0,0 +1,2 @@
+# Music Blocks
+The Blocks to be able to play Music in Discord Bot Builder
---
+++ Music Blocks/discord_audio_player_dependency.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: "Discord Audio Player [Dependency]",
+
+    description: "Starts the Discord Audio Player dependency required for other blocks to work.",
+
+    category: "Dependencies",
+
+    inputs: [],
+
+    options: [],
+
+    outputs: [],
+
+    async init(DBB) {
+        if (!DBB.Dependencies.DiscordPlayer)
+            DBB.Dependencies.DiscordPlayer = {}
+
+        const { Player } = require("discord-player");
+        DBB.Dependencies.DiscordPlayer.module = require("discord-player")
+
+        await DBB.Core.require('ffmpeg-static')
+        await DBB.Core.require('@discordjs/opus')
+        await DBB.Core.require("play-dl")
+
+        DBB.Dependencies.DiscordPlayer.player = new Player(DBB.DiscordJS.client);
+    },
+
+    code() { }
+}
